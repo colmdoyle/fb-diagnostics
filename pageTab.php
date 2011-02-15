8e362e0384205282ef06d649649e1c389f702677
Colm Doyle <colmdoyle@fb.com>
2011-02-15 12:30:39 +0000

    Added iFrame Page with Decoding

---
+++ pageTab.php
@@ -0,0 +1,36 @@
+
+<?php
+include('config.php');
+
+function parse_signed_request($signed_request, $secret) {
+  list($encoded_sig, $payload) = explode('.', $signed_request, 2); 
+
+  // decode the data
+  $sig = base64_url_decode($encoded_sig);
+  $data = json_decode(base64_url_decode($payload), true);
+
+  if (strtoupper($data['algorithm']) !== 'HMAC-SHA256') {
+    error_log('Unknown algorithm. Expected HMAC-SHA256');
+    return null;
+  }
+
+  // check sig
+  $expected_sig = hash_hmac('sha256', $payload, $secret, $raw = true);
+  if ($sig !== $expected_sig) {
+    error_log('Bad Signed JSON signature!');
+    return null;
+  }
+
+  return $data;
+}
+
+function base64_url_decode($input) {
+  return base64_decode(strtr($input, '-_', '+/'));
+}
+
+
+
+echo("<pre>\n");
+print_r(parse_signed_request($_REQUEST['signed_request'],$appsecret));
+echo("</pre>\n");
+?>
