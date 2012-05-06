3214a3a8ac42ce3d9f815236cade194548b6431a
Colm Doyle <colmdoyle@fb.com>
2012-05-06 03:45:02 +0100

    Added some dialogs. Should really pick between jquery and regular js

--- index.php
+++ index.php
@@ -30,10 +30,17 @@ $head = '<head>';
 $head .= output_standard_head('Colm\'s Sandbox', 'website', 'http://colmd.fbdublin.com/colms-sandbox/index.php', 'https://fbcdn-photos-a.akamaihd.net/photos-ak-snc7/v85005/230/120999667956026/app_10_120999667956026_1011543027.gif', 'Colm\'s Sandbox', $config['AppId']);
 $head .= '<script src="http://code.jquery.com/jquery-1.7.2.min.js"></script>';
 $head .= '<script src="scripts/main.js"></script>';
+$head .= '<script src=\'https://connect.facebook.net/en_US/all.js\'></script>';
 $head .= '<link rel="stylesheet" type="text/css" href="css/screen.css" />';
 $head .= '</head>';
 
+$fb_init .= '<div id=\'fb-root\'></div>';
+
+$welcome_header .= '<h1> Welcome to Colm\'s Sandbox </h1>';
+$welcome_header .= '<h2> Home of mediocre code since 2010 </h2>';
+
 $signed_request_div .= '<div id="signed_request">';
+$signed_request_div .= '<h3> Signed Request </h3>';
 $signed_request_div .= $signed_request_array;
 $signed_request_div .= '</div>';
 
@@ -44,10 +51,29 @@ $publish_action_div .= '</a>';
 $publish_action_div .= '<div id="action_response"></div>';
 $publish_action_div .= '</div>';
 
+$feed_dialog .= '<p><a onclick=\'postToFeed(); return false;\'>Post to Feed</a></p>';
+$oauth_dialog .= '<p><a onclick=\'authApp(); return false;\'>Auth App</a></p>';
+$add_page_tab_dialog .= '<p><a onclick=\'addToPage(); return false;\'>Add To Page</a></p>';
+$friends_dialog .= '';
+$pay_dialog .= '';
+$requests_dialog .= '<p><input type="button" onclick="sendRequestToRecipients(); return false;" value="Send Request to Users Directly" /><input type="text" value="User ID" name="user_ids" /></p><p><input type="button"  onclick="sendRequestViaMultiFriendSelector(); return false;" value="Send Request to Many Users with MFS"/></p>';
+$send_dialog .= '<p><a onclick=\'sendDialog(); return false;\'>Send Dialog</a></p>';
+
+$dialogs_div .= '<h3> Dialogs </h3> ';
+$dialogs_div .= $feed_dialog;
+$dialogs_div .= $oauth_dialog;
+$dialogs_div .= $add_page_tab_dialog;
+$dialogs_div .= $friends_dialog;
+$dialogs_div .= $pay_dialog;
+$dialogs_div .= $requests_dialog;
+$dialogs_div .= $send_dialog;
 
 
+
+$body .= $welcome_header;
 $body .= $signed_request_div;
 $body .= $publish_action_div;
+$body .= $dialogs_div;
 
 echo $head;
 echo $body;
