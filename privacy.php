3165b3a7aa7c9a17a2464cf2aa351edb58abde76
Colm Doyle <colmdoyle@fb.com>
2012-12-10 23:40:32 +0000

    Dynamically change privacy policy to include correct app name

--- privacy.php
+++ privacy.php
@@ -44,14 +44,14 @@ echo output_header();
 	<ol type="a">
 		<li>
 			Permission is granted to temporarily download one copy of the materials 
-			(information or software) on FB Diagnostics's web site for personal, 
+			(information or software) on <?php echo $config['AppName']; ?> web site for personal, 
 			non-commercial transitory viewing only. This is the grant of a license, 
 			not a transfer of title, and under this license you may not:
 			
 			<ol type="i">
 				<li>modify or copy the materials;</li>
 				<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
-				<li>attempt to decompile or reverse engineer any software contained on FB Diagnostics's web site;</li>
+				<li>attempt to decompile or reverse engineer any software contained on <?php echo $config['AppName']; ?> web site;</li>
 				<li>remove any copyright or other proprietary notations from the materials; or</li>
 				<li>transfer the materials to another person or "mirror" the materials on any other server.</li>
 			</ol>
@@ -67,7 +67,7 @@ echo output_header();
 	
 	<ol type="a">
 		<li>
-			The materials on FB Diagnostics's web site are provided "as is". FB Diagnostics makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, FB Diagnostics does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.
+			The materials on <?php echo $config['AppName']; ?> web site are provided "as is". FB Diagnostics makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, FB Diagnostics does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.
 		</li>
 	</ol>
 	
@@ -76,7 +76,7 @@ echo output_header();
 	</h3>
 	
 	<p>
-		In no event shall FB Diagnostics or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on FB Diagnostics's Internet site, even if FB Diagnostics or a FB Diagnostics authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+		In no event shall FB Diagnostics or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on <?php echo $config['AppName']; ?> Internet site, even if FB Diagnostics or a FB Diagnostics authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
 	</p>
 				
 	<h3>
@@ -84,7 +84,7 @@ echo output_header();
 	</h3>
 	
 	<p>
-		The materials appearing on FB Diagnostics's web site could include technical, typographical, or photographic errors. FB Diagnostics does not warrant that any of the materials on its web site are accurate, complete, or current. FB Diagnostics may make changes to the materials contained on its web site at any time without notice. FB Diagnostics does not, however, make any commitment to update the materials.
+		The materials appearing on <?php echo $config['AppName']; ?> web site could include technical, typographical, or photographic errors. FB Diagnostics does not warrant that any of the materials on its web site are accurate, complete, or current. FB Diagnostics may make changes to the materials contained on its web site at any time without notice. FB Diagnostics does not, however, make any commitment to update the materials.
 	</p>
 	
 	<h3>
@@ -108,7 +108,7 @@ echo output_header();
 	</h3>
 	
 	<p>
-		Any claim relating to FB Diagnostics's web site shall be governed by the laws of the State of California without regard to its conflict of law provisions.
+		Any claim relating to <?php echo $config['AppName']; ?> web site shall be governed by the laws of the State of California without regard to its conflict of law provisions.
 	</p>
 	
 	<p>
