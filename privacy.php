4e10491a4e4411f434c2d7898e23b118524bf503
Colm Doyle <colmdoyle@fb.com>
2012-12-10 23:06:07 +0000

    Change name in privacy boilerplate

--- privacy.php
+++ privacy.php
@@ -44,20 +44,20 @@ echo output_header();
 	<ol type="a">
 		<li>
 			Permission is granted to temporarily download one copy of the materials 
-			(information or software) on Colm's Sandbox's web site for personal, 
+			(information or software) on FB Diagnostics's web site for personal, 
 			non-commercial transitory viewing only. This is the grant of a license, 
 			not a transfer of title, and under this license you may not:
 			
 			<ol type="i">
 				<li>modify or copy the materials;</li>
 				<li>use the materials for any commercial purpose, or for any public display (commercial or non-commercial);</li>
-				<li>attempt to decompile or reverse engineer any software contained on Colm's Sandbox's web site;</li>
+				<li>attempt to decompile or reverse engineer any software contained on FB Diagnostics's web site;</li>
 				<li>remove any copyright or other proprietary notations from the materials; or</li>
 				<li>transfer the materials to another person or "mirror" the materials on any other server.</li>
 			</ol>
 		</li>
 		<li>
-			This license shall automatically terminate if you violate any of these restrictions and may be terminated by Colm's Sandbox at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
+			This license shall automatically terminate if you violate any of these restrictions and may be terminated by FB Diagnostics at any time. Upon terminating your viewing of these materials or upon the termination of this license, you must destroy any downloaded materials in your possession whether in electronic or printed format.
 		</li>
 	</ol>
 	
@@ -67,7 +67,7 @@ echo output_header();
 	
 	<ol type="a">
 		<li>
-			The materials on Colm's Sandbox's web site are provided "as is". Colm's Sandbox makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, Colm's Sandbox does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.
+			The materials on FB Diagnostics's web site are provided "as is". FB Diagnostics makes no warranties, expressed or implied, and hereby disclaims and negates all other warranties, including without limitation, implied warranties or conditions of merchantability, fitness for a particular purpose, or non-infringement of intellectual property or other violation of rights. Further, FB Diagnostics does not warrant or make any representations concerning the accuracy, likely results, or reliability of the use of the materials on its Internet web site or otherwise relating to such materials or on any sites linked to this site.
 		</li>
 	</ol>
 	
@@ -76,7 +76,7 @@ echo output_header();
 	</h3>
 	
 	<p>
-		In no event shall Colm's Sandbox or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on Colm's Sandbox's Internet site, even if Colm's Sandbox or a Colm's Sandbox authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
+		In no event shall FB Diagnostics or its suppliers be liable for any damages (including, without limitation, damages for loss of data or profit, or due to business interruption,) arising out of the use or inability to use the materials on FB Diagnostics's Internet site, even if FB Diagnostics or a FB Diagnostics authorized representative has been notified orally or in writing of the possibility of such damage. Because some jurisdictions do not allow limitations on implied warranties, or limitations of liability for consequential or incidental damages, these limitations may not apply to you.
 	</p>
 				
 	<h3>
@@ -84,7 +84,7 @@ echo output_header();
 	</h3>
 	
 	<p>
-		The materials appearing on Colm's Sandbox's web site could include technical, typographical, or photographic errors. Colm's Sandbox does not warrant that any of the materials on its web site are accurate, complete, or current. Colm's Sandbox may make changes to the materials contained on its web site at any time without notice. Colm's Sandbox does not, however, make any commitment to update the materials.
+		The materials appearing on FB Diagnostics's web site could include technical, typographical, or photographic errors. FB Diagnostics does not warrant that any of the materials on its web site are accurate, complete, or current. FB Diagnostics may make changes to the materials contained on its web site at any time without notice. FB Diagnostics does not, however, make any commitment to update the materials.
 	</p>
 	
 	<h3>
@@ -92,7 +92,7 @@ echo output_header();
 	</h3>
 	
 	<p>
-		Colm's Sandbox has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by Colm's Sandbox of the site. Use of any such linked web site is at the user's own risk.
+		FB Diagnostics has not reviewed all of the sites linked to its Internet web site and is not responsible for the contents of any such linked site. The inclusion of any link does not imply endorsement by FB Diagnostics of the site. Use of any such linked web site is at the user's own risk.
 	</p>
 	
 	<h3>
@@ -100,7 +100,7 @@ echo output_header();
 	</h3>
 	
 	<p>
-		Colm's Sandbox may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.
+		FB Diagnostics may revise these terms of use for its web site at any time without notice. By using this web site you are agreeing to be bound by the then current version of these Terms and Conditions of Use.
 	</p>
 	
 	<h3>
@@ -108,7 +108,7 @@ echo output_header();
 	</h3>
 	
 	<p>
-		Any claim relating to Colm's Sandbox's web site shall be governed by the laws of the State of California without regard to its conflict of law provisions.
+		Any claim relating to FB Diagnostics's web site shall be governed by the laws of the State of California without regard to its conflict of law provisions.
 	</p>
 	
 	<p>
